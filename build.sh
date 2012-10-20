b83d131439bd02a7f437b7193420611557ce71fd
Jari Aalto <jari.aalto@cante.net>
2012-10-20 22:43:46 +0300

    build.sh: g++

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# These are Gcc link flags
 LDFLAGS="-D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64"
+cc=${CXX:-g++}
 
-gcc --version | head -n 1
-gcc $CXXFLAGS $LDFLAGS -o kgb kgb_arch_posix_by_slawek.cpp
+$cc --version | head -n 1
+$cc $CXXFLAGS $LDFLAGS -o kgb kgb_arch_posix_by_slawek.cpp
+
+# End of file
